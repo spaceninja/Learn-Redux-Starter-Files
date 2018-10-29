3f4b5f0ff0690523e83341646084775fa0e936f5
Scott Vandehey <fake-scott@spaceninja.com>
2018-10-29 13:24:19 -0700

    03 creating single & photoGrid components

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return <div className="single-photo">I'm a single photo</div>;
+  }
+});
+
+export default Single;
