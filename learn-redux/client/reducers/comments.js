2c7d2e8f8bf4bd0f77fa09626289baaa32b40bc6
Scott Vandehey <fake-scott@spaceninja.com>
2018-10-30 11:18:50 -0700

    16 redux reducer composition

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,7 +2,39 @@
 // 1. the action (info about what happened)
 // 2. a copy of current state
 
+const postComments = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case 'REMOVE_COMMENT':
+      console.log('Removing a comment');
+      // return the new state without the deleted comment
+      return [
+        ...state.slice(0, action.i), // before the one we're updating
+        ...state.slice(action.i + 1) // after the one we're updating
+      ];
+    default:
+      return state;
+  }
+  return state;
+};
+
 const comments = (state = [], action) => {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 };
 
