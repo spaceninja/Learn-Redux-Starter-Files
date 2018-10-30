5871496e76923e46153f414aedc6b5dfbc25eb98
Scott Vandehey <fake-scott@spaceninja.com>
2018-10-30 13:04:53 -0700

    17 error tracking with sentry

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '1716f30e860e4e3f939e0e28a9c98a60';
+const sentry_app = '1312458';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -14,6 +14,16 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+// use sentry to monitor for errors
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'aasdfasdf9817-209348',
+    userLevel: 'editor'
+  }
+}).install();
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
